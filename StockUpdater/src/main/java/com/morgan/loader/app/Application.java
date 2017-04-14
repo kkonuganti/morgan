6cec0c387b031e62d3718aa7806f5a3267637688
kartheek.konganti <kartheek.konuganti@accenture.com>
2017-04-14 13:17:39 +0100

    changing the log lmit to 50
    Change-Id: I804d43dd0d80095164fa8ac796e57a7d7e085ca8

--- StockUpdater/src/main/java/com/morgan/loader/app/Application.java
+++ StockUpdater/src/main/java/com/morgan/loader/app/Application.java
@@ -39,7 +39,7 @@ public class Application {
         int mainCount=0;
         Scanner stdin = new Scanner(new BufferedInputStream(System.in));
         while (stdin.hasNext()) {
-        	if(mainCount==15){
+        	if(mainCount==50){
         		System.out.println("Not Accepting any more sales \n");
         		logger.logAdjustments();
         		System.out.println("\n Terminating the system \n");
